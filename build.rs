3f9868e5f6d88c77123ddf9ddb71f1a7bf3be601
Mikel Campo <mcampoe@securyblack.com>
2026-05-01 14:26:17 +0200

    build: use protoc-bin-vendored to avoid system protobuf dependency
    - Add protoc-bin-vendored to build-dependencies
    - Set PROTOC env var in build.rs from vendored binary
    - Remove manual protoc install steps from release workflow

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Use vendored protoc so CI builds don't depend on system protobuf
+    unsafe {
+        std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path().unwrap());
+    }
     tonic_build::compile_protos("proto/tunnel/v1/tunnel.proto")?;
     Ok(())
 }
